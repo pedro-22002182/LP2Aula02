d1208d1d9a0dfcc568c8fe18a732f00a035c1af1
PedroPassos <pedropassos.dev@gmail.com>
2021-10-05 21:09:47 +0100

    Add Class

---
+++ ClassVsSctruc/Player.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassVsSctruc
+{
+    public class Player
+    {
+        public float Health {get; set;}
+        public float Armor {get; set;}
+    }
+}
---
+++ ClassVsSctruc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassVsSctruc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1, p2;
+
+            p1 = new Player() {Health = 21.2f, Armor = 53.4f};
+            p2 = p1;
+
+            Console.WriteLine("Valores Inicias:");
+            Console.WriteLine($"\tP1: Vida = {p1.Health}, Armor = {p1.Armor}");
+            Console.WriteLine($"\tP2: Vida = {p2.Health}, Armor = {p2.Armor}");
+
+            p1.Health = 0; 
+            p1.Armor = 5;
+
+            Console.WriteLine("Valores Finais:");
+            Console.WriteLine($"\tP1: Vida = {p1.Health}, Armor = {p1.Armor}");
+            Console.WriteLine($"\tP2: Vida = {p2.Health}, Armor = {p2.Armor}");
+        }
+    }
+}
